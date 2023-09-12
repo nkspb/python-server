f36e72864fbf51c32befd32a44d078ad530c45f0
nkspb <nkspb13@gmail.com>
2023-09-12 03:46:40 +0300

    version 2.3

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ class MyServer(BaseHTTPRequestHandler):
         self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
         self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
         self.wfile.write(bytes("<body>", "utf-8"))
-        self.wfile.write(bytes("<p>This is an example web server version 2.2.</p>", "utf-8"))
+        self.wfile.write(bytes("<p>This is an example web server version 2.3.</p>", "utf-8"))
         self.wfile.write(bytes("</body></html>", "utf-8"))
 
 if __name__ == "__main__":        
